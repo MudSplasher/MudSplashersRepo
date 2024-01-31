cee499ea9bf3bb16f8d532fe9ea5057b39c56db4
MudSplasher <150399924+MudSplasher@users.noreply.github.com>
2024-01-31 18:31:27 -0500

    Add files via upload

---
+++ repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd $script_full_path || exit 1
+
+rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
+
+echo "[Repository] Generating Packages..."
+apt-ftparchive packages ./pool > Packages
+zstd -q -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
+
+echo "[Repository] Generating Release..."
+apt-ftparchive \
+		-o APT::FTPArchive::Release::Origin="MudSplashers Repo" \
+		-o APT::FTPArchive::Release::Label="MudSplashers Repo" \
+		-o APT::FTPArchive::Release::Suite="stable" \
+		-o APT::FTPArchive::Release::Version="2.0" \
+		-o APT::FTPArchive::Release::Codename="ios" \
+		-o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64" \
+		-o APT::FTPArchive::Release::Components="main" \
+		-o APT::FTPArchive::Release::Description="MudSplashers Repo " \
+		release . > Release
+
+echo "[Repository] Signing Release using Amy's GPG Key..."
+gpg -abs -u 816C7A50B575162DC29288CD72339224580758CE -o Release.gpg Release
+
+echo "[Repository] Finished"
